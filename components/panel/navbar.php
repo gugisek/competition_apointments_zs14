b37f3ce66cb7c1f99be1a557ffbe75d9dfa47ece
gugisek <gugisek@gmail.com>
2023-12-22 21:22:59 +0100

    add: counting to navbar

--- components/panel/navbar.php
+++ components/panel/navbar.php
@@ -85,6 +85,18 @@ while ($row = $result->fetch_assoc()) {
               </ul>';
             }elseif($_SESSION['account_type'] == 1){
               // administarator główny
+              $sql = "select count(id) from users;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $users = $row[0];
+              $sql = "select count(*) from schools;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $schools = $row[0];
+              $sql = "select count(*) from invitations;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $invitations = $row[0];
               echo '
               <ul role="list" class="-mx-2 space-y-1">
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="200">
@@ -103,7 +115,10 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                     </svg>
-                      Użytkownicy
+                      Użytkownicy';
+                      if($users > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$users.'</span>';
+                      }echo '
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="400">
@@ -112,7 +127,11 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                     </svg>
-                      Zaproszenia
+                      Zaproszenia';
+                      if($invitations > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$invitations.'</span>';
+                      }echo '
+
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="500">
@@ -121,7 +140,10 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
                     </svg>
-                      Szkoły
+                      Szkoły';
+                      if($schools > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$schools.'</span>';
+                      }echo '
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="600">
@@ -136,6 +158,34 @@ while ($row = $result->fetch_assoc()) {
               </ul>';
             }elseif($_SESSION['account_type'] == 2){
               // administator szkolny
+              $sql = "select school_id from users where id = $_SESSION[login_id];";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $school_id = $row[0];
+              $sql = "select count(*) from users where school_id = $school_id and role_id = 4;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $uczniowie = $row[0];
+              $sql = "select count(*) from users where school_id = $school_id and role_id = 3;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $nauczyciele = $row[0];
+              $sql = "select count(*) from user_class where school_id = $school_id;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $klasy = $row[0];
+              $sql = "select count(*) from przedmioty where school_id = $school_id;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $przedmioty = $row[0];
+              $sql = "select count(*) from rooms where school_id = $school_id;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $rooms = $row[0];
+              $sql = "select count(*) from invitations where school_id = $school_id;";
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_array($result);
+              $invitations = $row[0];
               echo '
               <ul role="list" class="-mx-2 space-y-1">
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="200">
@@ -154,7 +204,10 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                     </svg>
-                      Uczniowie
+                      Uczniowie';
+                      if($uczniowie > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$uczniowie.'</span>';
+                      }echo '
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="300">
@@ -163,7 +216,10 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
                     </svg>
-                      Nauczyciele
+                      Nauczyciele';
+                      if($nauczyciele > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$nauczyciele.'</span>';
+                      }echo '
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="300">
@@ -172,7 +228,10 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                     </svg>
-                      Klasy
+                      Klasy';
+                      if($klasy > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$klasy.'</span>';
+                      }echo '
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="300">
@@ -181,7 +240,10 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                     </svg>
-                      Przedmioty
+                      Przedmioty';
+                      if($przedmioty > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$przedmioty.'</span>';
+                      }echo '
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="300">
@@ -190,7 +252,10 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
                     </svg>
-                      Sale lekcyjne
+                      Sale lekcyjne';
+                      if($rooms > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$rooms.'</span>';
+                      }echo '
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="400">
@@ -199,7 +264,10 @@ while ($row = $result->fetch_assoc()) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                     </svg>
-                      Zaproszenia
+                      Zaproszenia';
+                      if($invitations > 0){
+                        echo '<span class="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-black/10 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-500 border border-[#3d3d3d]" aria-hidden="true">'.$invitations.'</span>';
+                      }echo '
                   </a>
                 </li>
                 <li onclick="mobileClose()" data-aos="fade-right" data-aos-delay="600">
